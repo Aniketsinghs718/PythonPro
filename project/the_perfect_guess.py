ff29641151941b4923faba065ec4710e0dc30e1f
Aniketsinghs718 <Aniketsinghs718@gmail.com>
2025-01-31 22:44:41 +0530

    project update

---
+++ project/the_perfect_guess.py
@@ -0,0 +1,8 @@
+import random
+
+# create a game that help to guess the number between 1 to 100
+# the program will generate a random number between 1 to 100 and ask the user to guess the number
+
+def game():
+    number=random.randint(0,100)
+    
