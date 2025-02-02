32a52ab41e7a126baec22462ebe09876029fe712
Aniketsinghs718 <Aniketsinghs718@gmail.com>
2025-02-02 17:33:03 +0530

    renaming

--- Jarvis/jarvis1/main1.py
+++ Jarvis/jarvis1/main1.py
@@ -3,4 +3,4 @@ import os
 import eel
 eel.init('web')
 os.system('start brave.exe --app="http://localhost:8000/index.html"')
-eel.start('index.html',size=(800,800), mode = None,host='localhost',block=True)
+eel.start('index.html',size=(1270,960), mode = None,host='localhost',block=True)
---
+++ Jarvis/jarvis1/web/main.js
@@ -0,0 +1,14 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     var siriWave = new SiriWave({
+//         container: document.getElementById("siri-container"),
+//         width: 400,
+//         height: 150,
+//         style: "ios9",
+//         amplitude: "0.6",
+//         frequency: "4",
+//         speed: "0.20",
+//         autostart: true
+//     });
+
+//     siriWave.start(); // Start the animation
+// });
