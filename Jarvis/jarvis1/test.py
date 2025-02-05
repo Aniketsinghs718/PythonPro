75162c4a81375953d37aaaba6f0e795d751a4354
Aniketsinghs718 <Aniketsinghs718@gmail.com>
2025-02-05 23:45:26 +0530

    demo dictionary code

--- Jarvis/jarvis1/test.py
+++ Jarvis/jarvis1/test.py
@@ -1,1 +1,11 @@
-print("hello")
+# print("hello")
+import os
+# f=open("demo.txt","r")
+# print(f.read())
+
+f=open("demo.txt","a")
+f.write("goog morning!!")
+f.close()
+
+f=open("demo.txt","r")
+print(f.read())
